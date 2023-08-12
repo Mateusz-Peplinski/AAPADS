bc113ea5695fafbca4e141ebcfd2d229b676308f
Mateusz <up2011287@myport.ac.uk>
2023-08-12 13:35:35 +0300

    window update

--- AAPADS/src/main/MainWindow.xaml.cs
+++ AAPADS/src/main/MainWindow.xaml.cs
@@ -1,53 +1,89 @@
 ﻿using AAPADS.src.engine;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace AAPADS
 {
-   
+
     public partial class MainWindow : Window
     {
-        private readonly DataIngestEngine dataIngestionEngineObject;
-        private readonly DetectionEngine detectionEngineObject;
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
 
-        private readonly overviewViewDataModel overviewViewDisplay;
+        [DllImport("User32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
 
-        private readonly detectionsViewDataModel detectionsDisplay;
-
-        private readonly networkAdapterInformationDataModel networkAdapterInformationDisplay;
+        private readonly DataIngestEngine DATA_INGESTION_ENGINE_OBJECT;
+        private readonly DetectionEngine DETECTION_ENGINE_OBJECT;
+        private readonly overviewViewDataModel OVERVIEW_VIEW_MODEL_OBJECT;
+        private readonly detectionsViewDataModel DETECTION_VIEW_MODEL_OBJECT;
+        private readonly networkAdapterInformationDataModel WLAN_NETWORK_ADAPTER_VIEW_MODEL_OBJECT;
+        
+        private double _originalWidth;
+        private double _originalHeight;
+        private double _originalLeft;
+        private double _originalTop;
+        private bool _wasMaximized = false;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            dataIngestionEngineObject = new DataIngestEngine();
-            overviewViewDisplay = new overviewViewDataModel();
-            dataIngestionEngineObject.SSIDDataCollected += UpdateOverviewTabUI;
-            dataIngestionEngineObject.Start();
+            SourceInitialized += (s, e) =>
+            {
+                IntPtr handle = (new WindowInteropHelper(this)).Handle;
+                HwndSource.FromHwnd(handle).AddHook(new HwndSourceHook(WindowProc));
+            };
 
-            detectionEngineObject = new DetectionEngine();
-            detectionsDisplay = new detectionsViewDataModel();
-            detectionEngineObject.DetectionDiscovered += updateDetectionTabUI;
-            detectionEngineObject.startdetection();
+            MinimizeButton.Click += (s, e) => WindowState = WindowState.Minimized;
 
-            DataContext = overviewViewDisplay;
+            MaximizeButton.Click += (s, e) => {
+                if (!_wasMaximized) 
+                {
+                    
+                    _originalWidth = Width;
+                    _originalHeight = Height;
+                    _originalLeft = Left;
+                    _originalTop = Top;
 
-            networkAdapterInformationDisplay = new networkAdapterInformationDataModel();
+                    
+                    WindowState = WindowState.Normal;
+                    Left = 0;
+                    Top = 0;
+                    Width = SystemParameters.WorkArea.Width;
+                    Height = SystemParameters.WorkArea.Height;
+
+                    _wasMaximized = true;
+                }
+                else 
+                {
+                    
+                    Width = _originalWidth;
+                    Height = _originalHeight;
+                    Left = _originalLeft;
+                    Top = _originalTop;
+
+                    _wasMaximized = false;
+                }
+            };
+
+            CloseButton.Click += (s, e) => Application.Current.Shutdown();
+
+            DATA_INGESTION_ENGINE_OBJECT = new DataIngestEngine();
+            OVERVIEW_VIEW_MODEL_OBJECT = new overviewViewDataModel();
+            DATA_INGESTION_ENGINE_OBJECT.SSIDDataCollected += UpdateOverviewTabUI;
+            DATA_INGESTION_ENGINE_OBJECT.Start();
+
+            DETECTION_ENGINE_OBJECT = new DetectionEngine();
+            DETECTION_VIEW_MODEL_OBJECT = new detectionsViewDataModel();
+            DETECTION_ENGINE_OBJECT.DetectionDiscovered += updateDetectionTabUI;
+            DETECTION_ENGINE_OBJECT.startdetection();
+
+            DataContext = OVERVIEW_VIEW_MODEL_OBJECT;
+
+            WLAN_NETWORK_ADAPTER_VIEW_MODEL_OBJECT = new networkAdapterInformationDataModel();
 
 
         }
@@ -64,35 +100,140 @@ namespace AAPADS
 
         private void overviewTab_Click(object sender, RoutedEventArgs e)
         {
-            DataContext = overviewViewDisplay;
+            DataContext = OVERVIEW_VIEW_MODEL_OBJECT;
         }
 
         private void detectionsTab_Click(object sender, RoutedEventArgs e)
         {
-            DataContext = detectionsDisplay;
+            DataContext = DETECTION_VIEW_MODEL_OBJECT;
 
-            
+
         }
         private void NetworkAdapter_Click(object sender, RoutedEventArgs e)
         {
-            DataContext = networkAdapterInformationDisplay;
-            
+            DataContext = WLAN_NETWORK_ADAPTER_VIEW_MODEL_OBJECT;
+
         }
 
         private void UpdateOverviewTabUI(object sender, EventArgs e)
         {
             this.Dispatcher.Invoke(() =>
             {
-                overviewViewDisplay.UpdateAccessPoints(dataIngestionEngineObject);
+                OVERVIEW_VIEW_MODEL_OBJECT.UpdateAccessPoints(DATA_INGESTION_ENGINE_OBJECT);
             });
         }
         private void updateDetectionTabUI(object sender, EventArgs e)
         {
             this.Dispatcher.Invoke(() =>
             {
-                detectionsDisplay.updateDetections(detectionEngineObject);
+                DETECTION_VIEW_MODEL_OBJECT.updateDetections(DETECTION_ENGINE_OBJECT);
             });
         }
+        private static IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case 0x0024:
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    handled = true;
+                    break;
+            }
+            return (IntPtr)0;
+        }
 
+        private static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
+        {
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+            if (monitor != IntPtr.Zero)
+            {
+                MONITORINFO monitorInfo = new MONITORINFO();
+                GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+            }
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+            public POINT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+            public RECT rcMonitor = new RECT();
+            public RECT rcWork = new RECT();
+            public int dwFlags = 0;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+            public static readonly RECT Empty = new RECT();
+            public int Width { get { return Math.Abs(right - left); } }
+            public int Height { get { return bottom - top; } }
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+            public RECT(RECT rcSrc)
+            {
+                left = rcSrc.left;
+                top = rcSrc.top;
+                right = rcSrc.right;
+                bottom = rcSrc.bottom;
+            }
+            public bool IsEmpty { get { return left >= right || top >= bottom; } }
+            public override string ToString()
+            {
+                if (this == Empty) { return "RECT {Empty}"; }
+                return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
+            }
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Rect)) { return false; }
+                return (this == (RECT)obj);
+            }
+
+            public override int GetHashCode() => left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
+
+            public static bool operator ==(RECT rect1, RECT rect2) { return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom); }
+
+            public static bool operator !=(RECT rect1, RECT rect2) { return !(rect1 == rect2); }
+        }
+
+       
     }
 }
