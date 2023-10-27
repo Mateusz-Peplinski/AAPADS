22c02bbe94055eeec0bfa0d9b5f400920b02b947
Mateusz <up2011287@myport.ac.uk>
2023-10-27 22:37:37 +0300

    setting ui and database settings

---
+++ AAPADS/src/dataModels/SettingsViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAPADS
+{
+    public class SettingsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+
+        private bool _isDebugConsoleEnabled;
+
+        public bool IsDebugConsoleEnabled
+        {
+            get { return _isDebugConsoleEnabled; }
+            set
+            {
+                if (_isDebugConsoleEnabled != value)
+                {
+                    _isDebugConsoleEnabled = value;
+
+                    if (_isDebugConsoleEnabled)
+                        ShowConsole();
+                    else
+                        HideConsole();
+
+                    OnPropertyChanged(nameof(IsDebugConsoleEnabled));
+                    SaveSettings();
+                }
+            }
+        }
+
+
+
+        public void LoadSettings()
+        {
+            using (var db = new SettingsDatabaseAccess("wireless_profile.db"))
+            {
+                var setting = db.GetSetting("DebugConsoleEnabled");
+                if (setting != null)
+                {
+                    IsDebugConsoleEnabled = setting == "true";
+                }
+            }
+
+            if (IsDebugConsoleEnabled)
+                ShowConsole();
+            else
+                HideConsole();
+        }
+
+
+        public void SaveSettings()
+        {
+            using (var db = new SettingsDatabaseAccess("wireless_profile.db"))
+            {
+                db.SaveSetting("DebugConsoleEnabled", IsDebugConsoleEnabled ? "true" : "false");
+            }
+        }
+
+
+
+
+        public void ShowConsole()
+        {
+            ShowWindow(GetConsoleWindow(), SW_SHOW);
+        }
+
+        public void HideConsole()
+        {
+            ShowWindow(GetConsoleWindow(), SW_HIDE);
+        }
+
+    }
+
+}
---
+++ AAPADS/src/databaseAccess/SettingsDatabaseAccess.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Dapper;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAPADS
+{
+    public class SettingsDatabaseAccess : IDisposable
+    {
+        private SQLiteConnection connection;
+        private readonly string dbPath;
+        public SQLiteConnection Connection => connection;
+
+        public SettingsDatabaseAccess(string dbFileName)
+        {
+            dbPath = Path.Combine(Directory.GetCurrentDirectory(), dbFileName);
+            connection = new SQLiteConnection($"Data Source={dbPath};Version=3;");
+            connection.Open();
+            CreateTablesIfNotExists();
+        }
+
+        private void CreateTablesIfNotExists()
+        {
+            var command = new SQLiteCommand(
+                @"CREATE TABLE IF NOT EXISTS ""Settings"" (
+                ""Key"" TEXT NOT NULL UNIQUE,
+                ""Value"" TEXT,
+                PRIMARY KEY(""Key"")
+            );", connection);
+
+            command.ExecuteNonQuery();
+        }
+
+        public string GetSetting(string key)
+        {
+            return connection.QueryFirstOrDefault<string>("SELECT Value FROM Settings WHERE Key = @Key", new { Key = key });
+        }
+
+        public void SaveSetting(string key, string value)
+        {
+            connection.Execute("INSERT OR REPLACE INTO Settings (Key, Value) VALUES (@Key, @Value)", new { Key = key, Value = value });
+        }
+
+        public void Dispose()
+        {
+            connection?.Dispose();
+        }
+    }
+
+}
