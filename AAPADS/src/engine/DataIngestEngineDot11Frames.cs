cba7dc1bb22f4b38ff017a81173259ff8f2db058
Mateusz <up2011287@myport.ac.uk>
2024-02-15 20:26:20 +0000

    frame capture engine

--- AAPADS/src/engine/DataIngestEngineDot11Frames.cs
+++ AAPADS/src/engine/DataIngestEngineDot11Frames.cs
@@ -12,7 +12,7 @@ namespace AAPADS
 {
     public class DataIngestEngineDot11Frames
     {
-        private static bool _stopCapturing;
+        private bool _stopCapturing;
 
         public DataIngestEngineDot11Frames()
         {
@@ -21,12 +21,12 @@ namespace AAPADS
 
 
 
-        public static void StartCaptureAsync()
+        public void StartCaptureAsync()
         {
             Task.Run(() => StartCapture());
         }
 
-        private static void StartCapture()
+        private void StartCapture()
         {
             Console.CancelKeyPress += HandleCancelKeyPress;
 
@@ -37,12 +37,24 @@ namespace AAPADS
                 Console.WriteLine("No devices were found on this machine");
                 return;
             }
+            int i = 0; 
+            foreach (var dev in devices) 
+            {
+                Console.WriteLine($"{i} - {dev}");
+                i++; // Increment i so that each device has a unique index
+            }
+            // Find a device that matches the name "WiFi 2"
+            var device = devices.FirstOrDefault(d => d.Name.Contains("WiFi 2"));
+            if (device == null)
+            {
+                Console.WriteLine("Device 'WiFi 2' not found on this machine");
+                return;
+            }
 
-            // Assuming the first device is the one we want to use
-            var device = devices[0];
+            // Open the device for capturing
             device.Open(DeviceModes.Promiscuous, 1000); // Open device with a read timeout of 1000 milliseconds
 
-            Console.WriteLine("-- Listening on {0}, hit 'ctrl-c' to stop...", device.Description);
+            Console.WriteLine($"-- Listening on {device.Description}, hit 'ctrl-c' to stop...");
 
             device.OnPacketArrival += new PacketArrivalEventHandler(Device_OnPacketArrival);
             device.StartCapture();
@@ -58,33 +70,35 @@ namespace AAPADS
             Console.WriteLine("-- Capture stopped");
         }
 
+
         private static void Device_OnPacketArrival(object s, PacketCapture e)
         {
-            //var packet = Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
-            //if (packet is IEEE80211Packet)
-            //{
-            //    var wifiPacket = (IEEE80211Packet)packet;
+            var packet = Packet.ParsePacket(e.GetPacket().LinkLayerType, e.GetPacket().Data);
 
-            //    // Check if this is a management frame
-            //    if (wifiPacket.FrameControl.SubType == FrameControlField.FrameSubTypes.ManagementBeacon ||
-            //        wifiPacket.FrameControl.SubType == FrameControlField.FrameSubTypes.ManagementAssociationRequest ||
-            //        wifiPacket.FrameControl.SubType == FrameControlField.FrameSubTypes.ManagementAssociationResponse 
-            //        // Add other management subtypes as needed
-            //        )
-            //    {
-            //        // Process the management frame
-            //        Console.WriteLine($"Management Frame captured: {wifiPacket.GetType()}");
-            //        
-            //        if (wifiPacket is DeauthenticationFrame deauthFrame)
-            //        {
-            //            Console.WriteLine($"Deauthentication Frame: Reason = {deauthFrame.Reason}");
-            //        }
-            //    }
-            //}
+            // Assuming the base packet is an 802.11 packet since we are in monitor mode
+            var wifiPacket = packet as PacketDotNet.Ieee80211.MacFrame;
+
+            if (wifiPacket != null)
+            {
+                // Process all the 802.11 packets here
+                switch (wifiPacket.FrameControl.SubType)
+                {
+                    case PacketDotNet.Ieee80211.FrameControlField.FrameSubTypes.ManagementBeacon:
+                        // Process Beacon Frame
+                        break;
+                    case PacketDotNet.Ieee80211.FrameControlField.FrameSubTypes.ManagementAssociationRequest:
+                        // Process Association Request Frame
+                        break;
+                    case PacketDotNet.Ieee80211.FrameControlField.FrameSubTypes.ManagementAssociationResponse:
+                        // Process Association Response Frame
+                        break;
+                      
+                }
+            }
         }
 
 
-        private static void HandleCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        private void HandleCancelKeyPress(object sender, ConsoleCancelEventArgs e)
         {
             Console.WriteLine("-- Stopping capture");
             _stopCapturing = true;
