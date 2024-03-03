b5d310cb546decbbd68aa281944ce349bead8b50
Mateusz <up2011287@myport.ac.uk>
2024-03-03 20:31:49 +0000

    GUI advancement for 802.11 Frame Capture

--- AAPADS/FrameInspectorViewModel.cs
+++ AAPADS/FrameInspectorViewModel.cs
@@ -1,14 +1,37 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace AAPADS
 {
     public class FrameInspectorViewModel : INotifyPropertyChanged
     {
+        private readonly DispatcherTimer _timer;
+        private int _framesThisSecond = 0;
+        public SeriesCollection SeriesCollection { get; set; }
+        public AxesCollection AxisYCollection { get; set; }
+        public AxesCollection AxisXCollection { get; set; }
         public ObservableCollection<FrameInfo> Frames { get; } = new ObservableCollection<FrameInfo>();
+
+        private int _frameCount;
+        public int FrameCount
+        {
+            get => _frameCount;
+            set
+            {
+                _frameCount = value;
+                OnPropertyChanged(nameof(FrameCount));
+            }
+        }
 
 
         public ICommand StartCaptureCommand { get; }
@@ -18,17 +41,85 @@ namespace AAPADS
         {
             StartCaptureCommand = new RelayCommand(StartCapture);
             StopCaptureCommand = new RelayCommand(StopCapture);
+            SeriesCollection = new SeriesCollection{
+                new LineSeries
+                {
+                    Values = new ChartValues<int>(),
+                    PointGeometry = null,
+                    Stroke = new SolidColorBrush(Color.FromRgb(110, 204, 37)),    
+                    LineSmoothness = 0,
+                    PointGeometrySize = 0,
+                    PointForeground =  new SolidColorBrush(Color.FromRgb(110, 204, 37)),
+                    Fill = Brushes.Transparent
+                }
+            };
+
+            AxisYCollection = new AxesCollection
+            {
+                new Axis
+                {
+                    Title = "Frames per Second",
+                    MinValue = 0 
+                }
+            };
+
+            AxisXCollection = new AxesCollection
+            {
+                new Axis
+                {
+                    Title = "Time (Seconds)",
+                    MinValue = 0 
+                }
+            };
+
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            _timer.Tick += TimerOnTick;
+        }
+        private void TimerOnTick(object sender, EventArgs e)
+        {
+            UpdateGraph(_framesThisSecond);
+            _framesThisSecond = 0; 
+        }
+        public void OnFrameCaptured()
+        {
+            _framesThisSecond++;
+            
+            FrameCount++;
+        }
+        public void UpdateGraph(int newFramesCount)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                var series = SeriesCollection[0];
+                series.Values.Add(newFramesCount);
+
+                if (series.Values.Count > 60)
+                {
+                    series.Values.RemoveAt(0);
+                }
+
+                OnPropertyChanged(nameof(SeriesCollection));
+            });
+        }
+
+
+        private IList<string> GenerateLabels(int count)
+        {
+            return Enumerable.Range(1, count).Select(x => x.ToString()).ToList();
         }
 
         private void StartCapture()
         {
-            // Logic to start capture
+            _timer.Start();
             DataIngestEngineDot11Frames.Instance?.StartCaptureAsync();
         }
 
         private void StopCapture()
         {
-            // Logic to stop capture
+            _timer.Stop();
             DataIngestEngineDot11Frames.Instance?.StopCapture();
         }
 
